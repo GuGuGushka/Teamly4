8bd7af8eb2e2e5345e9aed9dce121ecfc8ac84e3
GuGuGushka <149265975+GuGuGushka@users.noreply.github.com>
2025-04-02 16:12:09 +0300

    ww

--- Teamly4/WorkerMainWindow.xaml.cs
+++ Teamly4/WorkerMainWindow.xaml.cs
@@ -19,9 +19,63 @@ namespace Teamly4
     /// </summary>
     public partial class WorkerMainWindow : Window
     {
+        private readonly IUM2323DTeamlyEntities db;
+        private readonly Users currentUser;
         public WorkerMainWindow()
         {
             InitializeComponent();
+            db = new IUM2323DTeamlyEntities();
+            currentUser = db
+                        .Users
+                        .FirstOrDefault(u => u.UserName == CurrentUserGlobal.Login && u.Password == CurrentUserGlobal.Password);
+
+            LoadProjectData();
+        }
+
+        private void LoadProjectData()
+        {
+            try
+            {
+                DataGridWorkerProjects.ItemsSource = db.Projects.Where(q => q.Performers.Any(p => p.Workerid == currentUser.Id)).ToList();
+
+                ComboBoxPriorities.ItemsSource = db.Priorities.ToList();
+                ComboBoxStatuses.ItemsSource = db.Statuses.Where(t => t.Id != 1002).ToList();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void DataGridWorkerProjects_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+
+        }
+
+        private void DataGridTasks_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ButtonFinishTask_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ComboBoxPriorities_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBoxStatuses_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
